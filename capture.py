d7ab6c1ce9a06de962fa12f7db33ebc87a1523e6
Steve Dodd <sdodd@salesforce.com>
2020-08-25 19:20:55 -0600

    WIP: Flask conversion, pie chart not displaying

--- capture.py
+++ capture.py
@@ -2,6 +2,7 @@
 
 import json
 import requests
+from flask import Flask, Markup, render_template
 
 '''
 Background:
@@ -23,8 +24,10 @@ If possible provide automated testing for your code.
 If possible put the app on AWS or favorite cloud service.
 '''
 
-def main():
-    asn = '46489'
+app = Flask(__name__)
+
+@app.route('/<int:asn>')
+def report(asn):
     r = requests.get(f'https://peeringdb.com/api/net?asn={asn}&depth=2')
     ixs = json.loads(r.text)
 
@@ -48,5 +51,12 @@ def main():
     print('Unique Peerings', len(ix_aggregate))
     print('Total Speed', total_aggregate_speed)
 
+    return render_template('report.html', title='Peering Bandwith per IX', ix_aggregate=ix_aggregate, asn=asn,
+                           total_bw=total_aggregate_speed, total_peers=len(ixs['data'][0]['netixlan_set']),
+                           unique_peers=len(ix_aggregate))
+
+def main():
+    app.debug = True
+    app.run(host='0.0.0.0', port=80)
 
 main()
