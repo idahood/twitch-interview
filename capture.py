2e167b3492685918e15adf9c2c1b75fa9934e802
Steve Dodd <sdodd@salesforce.com>
2020-08-25 21:35:59 -0600

    Add error handling for invalid ASNs

--- capture.py
+++ capture.py
@@ -2,7 +2,7 @@
 
 import json
 import requests
-from flask import Flask, Markup, render_template
+from flask import Flask, Markup, abort, render_template
 
 '''
 Background:
@@ -11,7 +11,7 @@ Your CEO has asked you to produce a report showing all our public peering exchan
 
 Requirements:
 
-Please write an application using peeringdb API(https://peeringdb.com/apidocs/).
+Please write an application using peeringdb API (https://peeringdb.com/apidocs/)
 Your CEO would like to see a list of all the public peerings grouped by peering exchange point name.
 He would also like an executive summary showing total peerings, total unique organization peerings, and total aggregate speed.
 
@@ -28,26 +28,31 @@ app = Flask(__name__)
 
 @app.route('/<int:asn>')
 def report(asn):
-    r = requests.get(f'https://peeringdb.com/api/net?asn={asn}&depth=2')
-    ixs = json.loads(r.text)
+    response = requests.get(f'https://peeringdb.com/api/net?asn={asn}&depth=2')
 
-    ix_aggregate = {}
-    total_aggregate_speed = 0
+    if response.status_code == 200:
+        ixs = json.loads(response.text)
 
-    for item in ixs['data'][0]['netixlan_set']:
-        ix = item['name']
-        speed = item['speed']
+        ix_aggregate = {}
+        total_aggregate_speed = 0
 
-        total_aggregate_speed += speed
+        for item in ixs['data'][0]['netixlan_set']:
+            ix = item['name']
+            speed = item['speed']
 
-        if ix in ix_aggregate:
-            ix_aggregate[ix] += speed
-        else:
-            ix_aggregate[ix] = speed
+            total_aggregate_speed += speed
 
-    return render_template('report.html', title='Peering Bandwith per IX', ix_aggregate=ix_aggregate, asn=asn,
-                           total_bw=total_aggregate_speed, total_peers=len(ixs['data'][0]['netixlan_set']),
-                           unique_peers=len(ix_aggregate))
+            if ix in ix_aggregate:
+                ix_aggregate[ix] += speed
+            else:
+                ix_aggregate[ix] = speed
+
+        return render_template('report.html', ix_aggregate=ix_aggregate, asn=asn,
+                               total_bw=total_aggregate_speed,
+                               total_peers=len(ixs['data'][0]['netixlan_set']),
+                               unique_peers=len(ix_aggregate))
+    else:
+        abort(response.status_code)
 
 def main():
     app.debug = True
