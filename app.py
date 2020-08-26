41625370d302d8f05c87df16907d451aff57d359
Steve Dodd <sdodd@salesforce.com>
2020-08-26 14:00:22 -0600

    Update docs

--- app.py
+++ app.py
@@ -1,27 +1,6 @@
 import json
-import os
 import requests
 from flask import Flask, Markup, abort, render_template
-
-'''
-Background:
-
-Your CEO has asked you to produce a report showing all our public peering exchange points for AS46489 and create an executive summary for him.
-
-Requirements:
-
-Please write an application using peeringdb API (https://peeringdb.com/apidocs/)
-Your CEO would like to see a list of all the public peerings grouped by peering exchange point name.
-He would also like an executive summary showing total peerings, total unique organization peerings, and total aggregate speed.
-
-The CEO is known for being impressed if any other useful information is also shown in the executive summary.
-
-Bonus:
-
-If possible create a database backed web application to display the data.
-If possible provide automated testing for your code.
-If possible put the app on AWS or favorite cloud service.
-'''
 
 app = Flask(__name__)
 
